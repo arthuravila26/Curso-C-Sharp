a292d351781b8728e127393274fdeedc2827237c
arthuravila26 <54253576+arthuravila26@users.noreply.github.com>
2019-10-22 16:31:38 -0300

    Add files via upload

---
+++ While/While/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = 1;
+            while (numero <= 10)
+            {
+                listBox1.Items.Add(numero);
+                numero = numero + 1;
+
+                /* Declaração da variável numero do tipo inteiro,
+                 * a variável foi inicializada com número 1.
+                 * "Enquanto" o numero for menor ou igual a 5 o
+                 * listBox será preenchido
+                 * expressão = numero + numero1 é o incremento */
+            }
+        }
+    }
+}
